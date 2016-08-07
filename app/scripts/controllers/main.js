4d396d74c17a41235ba9c62e8813a06ff1b18931
brantje <brantje@gmail.com>
2016-08-08 01:04:22 +0200

    Define vars

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,11 @@ angular.module('pokemonGoWebViewApp')
   .controller('MainCtrl', ['$scope', 'NgMap', '$rootScope', function ($scope, NgMap, $rootScope) {
     $scope.sidebar_shown = true;
     $scope.maps = [];
+
+    $scope.active_bot = null;
+    $scope.events = [];
+    $scope.bots = [];
+
     $scope.$on('mapInitialized', function(event, map) {
       $scope.maps.push(map)
     })
