42a8e2ef55311494893fc61b179ea741d6418e74
jasper <jasper@vanderstoop.nl>
2016-08-08 05:02:43 +0200

    - Added socket.io listener - Currently listens for pokemon caught and pokestops spun, those are shown in the dropdown menu in the header - Added pokemon gifs - Replaced 3d pokemons for 2d pokemons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngMap'
+    'ngMap',
+    'btford.socket-io'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -28,4 +29,12 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .factory('mySocket', function (socketFactory) {
+    var myIoSocket = io.connect('localhost:4001');
+
+    return socketFactory({
+      ioSocket: myIoSocket
+    });
+  })
+  ;
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,23 @@
  * Controller of the pokemonGoWebViewApp
  */
 angular.module('pokemonGoWebViewApp')
-  .controller('MainCtrl', ['$scope', 'NgMap', '$rootScope', function ($scope, NgMap, $rootScope) {
+  .controller('MainCtrl', ['$scope', 'NgMap', '$rootScope', 'socketFactory', 'mySocket', function ($scope, NgMap, $rootScope, socketFactory, mySocket) {
     $scope.sidebar_shown = true;
     $scope.maps = [];
     $scope.$on('mapInitialized', function(event, map) {
       $scope.maps.push(map)
-    })
+    });
 
+    $scope.pokestops = [];
+
+    mySocket.on('spun_pokestop:anon@gmail.com', function (data) {
+      $scope.pokestops.push(data.pokestop);
+    });
+
+    $scope.pokemons = [];
+
+    mySocket.on('pokemon_caught:anon@gmail.com', function (data) {
+      var pokemon = {name: data.pokemon, desc: data.iv_display};
+      $scope.pokemons.push(pokemon);
+    });
   }]);
