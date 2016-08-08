9e490f3857bbcd05b814952ada6ef3c54f10dc97
brantje <brantje@gmail.com>
2016-08-08 17:33:28 +0200

    Add Eventservice

---
+++ app/scripts/services/eventservice.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pokemonGoWebViewApp.EventService
+ * @description
+ * # EventService
+ * Service in the pokemonGoWebViewApp.
+ */
+angular.module('pokemonGoWebViewApp')
+  .service('EventService',['$rootScope', function ($rootScope) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    var socket = io.connect('http://' + document.domain + ':4000');
+
+    var onevent = socket.onevent;
+    socket.onevent = function (packet) {
+      var args = packet.data || [];
+      onevent.call (this, packet);    // original call
+      packet.data = ["*"].concat(args);
+      onevent.call(this, packet);      // additional call to catch-all
+    };
+
+    socket.on("*",function(event,data) {
+      console.log('Socket IO Event: ', event);
+      console.log('Socket IO data: ', data);
+    });
+
+    return {
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        })
+      }
+    };
+  }]);
