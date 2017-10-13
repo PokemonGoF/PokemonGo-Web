d69e8397127e7b37d18052a257efaff8ee1bd3a7
murluck <murluck.s@gmail.com>
2017-10-13 18:45:47 +0300

    fixed the state when user enable = false or enableSocket = false it keept trying to connect to socket

--- js/users_socket_io.js
+++ js/users_socket_io.js
@@ -6,8 +6,11 @@ SocketIOUsers.prototype.init = function (users, mapView) {
 
     for (var key in users) {
         if (users.hasOwnProperty(key)) {
-            var element = users[key];
-            usersSocket[key] = this.createUser(element, mapView);
+            var user = users[key];
+            var userSocket = this.createUser(user, mapView);
+
+            if (usersSocket != undefined && usersSocket != null && typeof userSocket != "undefined")
+                usersSocket[key] = usersSocket
         }
     }
 
@@ -16,18 +19,23 @@ SocketIOUsers.prototype.init = function (users, mapView) {
 
 //user is element from the userInfo.users object
 SocketIOUsers.prototype.createUser = function (user, self) {
+    if (!user.enable || !user.enableSocket)
+        return undefined;
 
     var retry_time = 30,
         prevMsg = '',
         timeOut = 5000,
         bgColor = '',
         eventsColors = self.getEventsColors(),
+        isConnected = false,
         logThis = /(egg_hatched|pokemon_appeared|pokemon_caught|pokemon_fled|pokemon_vanished|vip_pokemon|level_up|bot_sleep|show_best_pokemon|show_inventory|no_pokeballs|bot_sleep|bot_random_pause|api_error|pokemon_release|future_pokemon_release|bot_random_alive_pause|next_egg_incubates|spun_pokestop|path_lap_end|gained_candy|used_lucky_egg|lured_pokemon_found|softban|pokemon_inventory_full|inventory_full|buddy_next_reward|buddy_candy_earned|buddy_pokemon|buddy_update|buddy_reward|buddy_walked)/;
 
     var user_io = io.connect(user.socketAddress, { 'reconnectionAttempts': 5 });
 
     user_io.on('connect', function (event) {
         var thisSocket = this;
+        isConnected = true;
+
         self.log({
             message: "<span style='color: green;'><b>Connected to '" + thisSocket.io.uri + "'...</b></span>",
             timeout: 3000
@@ -35,6 +43,7 @@ SocketIOUsers.prototype.createUser = function (user, self) {
     });
 
     user_io.on('disconnect', function (event) {
+        isConnected = false;
         var thisSocket = this;
         self.log({
             message: "<span style='color: red;'><b>Disconnected from '" + thisSocket.io.uri + "'... Trying to reconnect, please wait...</b></span>",
@@ -44,15 +53,18 @@ SocketIOUsers.prototype.createUser = function (user, self) {
 
     user_io.on('reconnect_failed', function (event) {
         var thisSocket = this;
-        self.log({
-            message: "<span style='color: red;'><b>Connecting to '" + thisSocket.io.uri + "' failed. Retrying after " + retry_time + " seconds.</b></span>",
-            timeout: 3000
-        });
-        setTimeout(function () {
-            reconnectSocket();
-        }, retry_time * 1000);
+        if (!isConnected) {
+            self.log({
+                message: "<span style='color: red;'><b>Connecting to '" + thisSocket.io.uri + "' failed. Retrying after " + retry_time + " seconds.</b></span>",
+                timeout: 3000
+            });
+
+            setTimeout(function () {
+                reconnectSocket();
+            }, retry_time * 1000);
+        }
     });
-    
+
     function reconnectSocket() {
         self.log({
             message: "<span style='color: blue;'><b>Reconnecting to " + user_io.io.uri + "...</b></span>",
@@ -91,7 +103,7 @@ SocketIOUsers.prototype.createUser = function (user, self) {
                 });
             }
         }
-    }    
+    }
 
     return user_io;
 };
